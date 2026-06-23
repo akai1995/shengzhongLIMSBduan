bc43a55950b7df1f28ad9149c54b399bb176646e
lks <1471519969@qq.com>
2026-06-23 10:27:09 +0800

    feat: update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: '0.0.0.0',
-        port: 5173
-    }
+export default defineConfig(function (_a) {
+    var command = _a.command;
+    var isBuild = command === 'build';
+    return {
+        plugins: [react()],
+        server: {
+            host: '0.0.0.0',
+            port: 5173
+        },
+        base: '/'
+    };
 });
--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,6 @@ export default defineConfig(({ command }) => {
       host: '0.0.0.0',
       port: 5173
     },
-    base: isBuild ? '/shengzhongLIMSBduan/' : '/'
+    base: '/'
   }
 })
